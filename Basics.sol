c7e215d934659be7d3178d0d53b670681782c510
0xcontrib <0xcontrib@gmail.com>
2022-03-26 16:59:38 +0530

    Initial commit

---
+++ Basics.sol
@@ -0,0 +1,49 @@
+// pragma specifies the compiler version of Solidity
+
+
+/* Variables
+
+There are 3 types of variables in Solidity
+
+local 
+- declared inside a function
+- not stored on the blockchain
+
+state
+- declared outside a function
+- stored on the blockchain
+
+global
+- provides information about the blockchain
+
+*/
+
+/* Constants
+
+Constants are variables that cannot be modified.
+
+Their value is hard coded and using constants can save gas cost.
+
+*/
+
+
+/* Immutable Variables
+
+Immutable variables are like constants. 
+Values of immutable variables can be set inside the constructor but cannot be modified afterwards.
+
+*/
+
+/* Reading and Writing to a State Variable
+
+To write or update a state variable you need to send a transaction. 
+On the other hand, you can read state variables, for free, without any transaction fee. 
+
+*/
+
+/* Ether and Wei 
+
+Transactions are paid with ether. 
+Similar to how one dollar is equal to 100 cent, one ether is equal to 10^18 wei. 
+
+*/
